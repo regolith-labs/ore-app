f129a7f6506ad7317f17ce75e86a84b0b946a12e
Hardhat Chad <hardhatchad@gmail.com>
2024-02-29 15:40:35 -0600

    rounded

--- src/components/tutorial.rs
+++ src/components/tutorial.rs
@@ -4,7 +4,7 @@ use dioxus::prelude::*;
 pub fn Tutorial(cx: Scope) -> Element {
     render! {
         div {
-            class: "absolute right-8 bottom-20 px-3 py-2 animate-bounce bg-green-500 text-white rounded-sm shadow-sm",
+            class: "absolute right-8 bottom-20 px-3 py-2 animate-bounce bg-green-500 text-white rounded shadow-sm",
             p {
                 class: "font-medium",
                 "Click here to start mining."
