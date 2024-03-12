f84e4cc8a6b9e2be7d77fa9c99652872bb3224ae
Hardhat Chad <hardhatchad@gmail.com>
2024-03-12 10:58:18 -0500

    footer

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -7,7 +7,7 @@ use crate::components::{GithubIcon, OreLogoIcon, XIcon};
 pub fn Footer(cx: Scope) -> Element {
     render! {
         div {
-            class: "flex flex-row bg-black text-white w-full py-10 px-4 sm:px-8 justify-between",
+            class: "flex flex-row bg-black text-white w-full py-6 sm:py-10 px-4 sm:px-8 justify-between",
             OreLogoIcon {
                 class: "h-6 md:h-8 my-auto"
             }
