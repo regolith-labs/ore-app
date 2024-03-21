117f0d11ec309d01197d4023176d628309e97347
Hardhat Chad <hardhatchad@gmail.com>
2024-03-21 14:30:48 -0500

    fix desktop tailwind config

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn main() {
     dioxus_desktop::launch_cfg(
         App,
         dioxus_desktop::Config::new()
-            .with_custom_head(r#"<link rel="stylesheet" href="public/tailwind.css">"#.to_string()),
+            .with_custom_head(r#"<link rel="stylesheet" href="tailwind.css">"#.to_string()),
     );
 }
 
