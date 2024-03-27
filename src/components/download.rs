f3567bd9ae8d489c2182d85e1ec640a7c0db9f51
Hardhat Chad <hardhatchad@gmail.com>
2024-03-27 14:27:25 -0500

    add solana-keygen to cli download instructions

--- src/components/download.rs
+++ src/components/download.rs
@@ -49,10 +49,11 @@ pub fn Download(cx: Scope) -> Element {
                     text: "curl https://sh.rustup.rs -sSf | sh"
                 }
                 p {
-                    "Next, install the Solana CLI if you haven't done so already. "
+                    "Next, install the Solana CLI and create a Solana keypair if you haven't done so already. "
                 }
                 CodeBlock {
-                    text: "sh -c \"$(curl -sSfL https://release.solana.com/v1.18.4/install)\""
+                    text: "sh -c \"$(curl -sSfL https://release.solana.com/v1.18.4/install)\"
+                    solana-keygen new"
                 }
                 p {
                     "Now, install the Ore CLI."
