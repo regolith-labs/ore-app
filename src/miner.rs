ebb8fa067b23c9fd82044dda9e7c10a1e367e7d6
Hardhat Chad <hardhatchad@gmail.com>
2024-03-28 14:26:50 -0500

    move to async mining task

--- src/miner.rs
+++ src/miner.rs
@@ -81,7 +81,7 @@ impl Miner {
         // TODO interrupt current work (optimization)
     }
 
-    pub fn start_mining(&self, hash: KeccakHash, difficulty: KeccakHash, signer: Pubkey) {
+    pub async fn start_mining(&self, hash: KeccakHash, difficulty: KeccakHash, signer: Pubkey) {
         #[cfg(feature = "web")]
         {
             self.worker
@@ -98,7 +98,6 @@ impl Miner {
                 .unwrap();
         }
 
-        // TODO Configurable power level
         #[cfg(feature = "desktop")]
         {
             let ch = self.ch.clone();
@@ -107,28 +106,55 @@ impl Miner {
             let power_percent = ((self.power_level.read().0 + 1) as f64) / 8f64;
             let concurrency = num_cpus::get() as u64;
             let tuned_concurrency = ((concurrency as f64) * power_percent).round() as u64;
-            let handles: Vec<_> = (0..tuned_concurrency)
-                .map(|i| {
-                    std::thread::spawn({
-                        let flag = flag.clone();
-                        let result = result.clone();
-                        move || {
-                            let nonce =
-                                u64::MAX.saturating_div(tuned_concurrency).saturating_mul(i);
-                            if let Some(res) =
-                                find_next_hash_par(hash, difficulty, signer, nonce, flag.clone())
-                            {
-                                flag.store(true, Ordering::Relaxed);
-                                let mut w_result = result.lock().unwrap();
-                                *w_result = res;
-                            }
+            let mut tasks = Vec::new();
+            for i in 0..tuned_concurrency {
+                let flag = flag.clone();
+                let result = result.clone();
+                let task = async_std::task::spawn({
+                    let flag = flag.clone();
+                    let result = result.clone();
+                    async move {
+                        let nonce = u64::MAX.saturating_div(tuned_concurrency).saturating_mul(i);
+                        if let Some(res) =
+                            find_next_hash_par(hash, difficulty, signer, nonce, flag.clone())
+                        {
+                            flag.store(true, Ordering::Relaxed);
+                            let mut w_result = result.lock().unwrap();
+                            *w_result = res;
                         }
-                    })
-                })
-                .collect();
-            for h in handles {
-                h.join().unwrap();
+                    }
+                });
+                tasks.push(task);
             }
+
+            // let handles: Vec<_> = (0..tuned_concurrency)
+            //     .map(|i| {
+            //         std::thread::spawn({
+            //             let flag = flag.clone();
+            //             let result = result.clone();
+            //             move || {
+            //                 let nonce =
+            //                     u64::MAX.saturating_div(tuned_concurrency).saturating_mul(i);
+            //                 if let Some(res) =
+            //                     find_next_hash_par(hash, difficulty, signer, nonce, flag.clone())
+            //                 {
+            //                     flag.store(true, Ordering::Relaxed);
+            //                     let mut w_result = result.lock().unwrap();
+            //                     *w_result = res;
+            //                 }
+            //             }
+            //         })
+            //     })
+            //     .collect();
+            // for h in handles {
+            //     h.join().unwrap();
+            // }
+
+            // Await all tasks to complete
+            for task in tasks {
+                task.await;
+            }
+
             let r_result = result.lock().unwrap();
             let res = r_result.clone();
             async_std::task::spawn(async move {
@@ -149,10 +175,8 @@ fn find_next_hash_par(
     let mut next_hash: KeccakHash;
     let mut nonce = nonce;
     loop {
-        if nonce % 10_000 == 0 {
-            if flag.load(Ordering::Relaxed) {
-                return None;
-            }
+        if nonce % 10_000 == 0 && flag.load(Ordering::Relaxed) {
+            return None;
         }
         next_hash = hashv(&[
             hash.as_ref(),
