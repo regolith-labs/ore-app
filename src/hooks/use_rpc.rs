1d29df78331d29f16e48341b01386f115c47dd63
Hardhat Chad <hardhatchad@gmail.com>
2024-04-03 10:49:19 -0500

    migrate old stored rpc value to new

--- src/hooks/use_rpc.rs
+++ src/hooks/use_rpc.rs
@@ -8,9 +8,12 @@ const KEY: &str = "rpc";
 #[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq)]
 pub struct RpcUrl(pub String);
 
+const OLD_RPC_URL: &str = "https://amaleta-5y8tse-fast-mainnet.helius-rpc.com/";
+
 pub fn use_rpc_url(cx: &ScopeState) -> &UseSharedState<RpcUrl> {
     let rpc = use_shared_state::<RpcUrl>(cx).unwrap();
     let rpc_persistent = use_persistent(cx, KEY, || RpcUrl(RPC_URL.to_string()));
+
     use_effect(cx, rpc, |_| {
         rpc_persistent.set(rpc.read().clone());
         async move {}
@@ -19,6 +22,14 @@ pub fn use_rpc_url(cx: &ScopeState) -> &UseSharedState<RpcUrl> {
 }
 
 pub fn use_rpc_url_provider(cx: &ScopeState) {
-    let rpc = use_persistent(cx, KEY, || RpcUrl(RPC_URL.to_string())).get();
-    use_shared_state_provider(cx, || rpc);
+    let rpc = use_persistent(cx, KEY, || RpcUrl(RPC_URL.to_string()));
+
+    use_effect(cx, (), |_| {
+        if rpc.get().0.eq(&OLD_RPC_URL) {
+            rpc.set(RpcUrl(RPC_URL.to_string()));
+        }
+        async move {}
+    });
+
+    use_shared_state_provider(cx, || rpc.get());
 }
