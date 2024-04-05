c0f0efe54bdb3dfecf0d5e9a9b81e9277aa430ce
Hardhat Chad <hardhatchad@gmail.com>
2024-04-05 10:01:05 -0500

    bump compute budget

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -79,10 +79,10 @@ use crate::metrics::{track, AppEvent};
 pub const API_URL: &str = "https://ore-api-lthm.onrender.com";
 pub const RPC_URL: &str = "https://rpc.ironforge.network/mainnet?apiKey=01HTD8PPGDM1JBVQVEVJKXZ47F";
 
-pub const CU_LIMIT_REGISTER: u32 = 7660;
+pub const CU_LIMIT_REGISTER: u32 = 12_000; // 7660;
 pub const CU_LIMIT_CLAIM: u32 = 11_000;
 pub const CU_LIMIT_TRANSFER: u32 = 30_000; // TODO
-pub const CU_LIMIT_ATA: u32 = 24_000;
+pub const CU_LIMIT_ATA: u32 = 60_000; // 27_000;
 pub const CU_LIMIT_RESET: u32 = 12_200;
 pub const CU_LIMIT_MINE: u32 = 3200;
 
