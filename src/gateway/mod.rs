fa170b90339f72a3d868298b0641cf1879231d30
Hardhat Chad <hardhatchad@gmail.com>
2024-04-05 10:07:51 -0500

    bump claim cu budget

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -80,7 +80,7 @@ pub const API_URL: &str = "https://ore-api-lthm.onrender.com";
 pub const RPC_URL: &str = "https://rpc.ironforge.network/mainnet?apiKey=01HTD8PPGDM1JBVQVEVJKXZ47F";
 
 pub const CU_LIMIT_REGISTER: u32 = 12_000; // 7660;
-pub const CU_LIMIT_CLAIM: u32 = 11_000;
+pub const CU_LIMIT_CLAIM: u32 = 20_000; // 11_000;
 pub const CU_LIMIT_TRANSFER: u32 = 30_000; // TODO
 pub const CU_LIMIT_ATA: u32 = 60_000; // 27_000;
 pub const CU_LIMIT_RESET: u32 = 12_200;
