becc105ced3ecf3e27d035db967c6f2d77e8c91d
Hardhat Chad <hardhatchad@gmail.com>
2024-06-07 17:01:00 -0700

    get browser mining working

--- src/components/stop_button.rs
+++ src/components/stop_button.rs
@@ -9,15 +9,14 @@ use crate::{
 };
 
 pub fn StopButton() -> Element {
-    let miner = use_miner();
     let mut toolbar_state = use_miner_toolbar_state();
     rsx! {
         button {
             class: "transition transition-colors flex-shrink-0 flex w-10 h-10 justify-center rounded-full hover:bg-green-600 active:bg-green-700",
             title: "Stop mining",
             onclick: move |e| {
+                // miner.read().stop();
                 toolbar_state.pause();
-                miner.read().stop();
                 e.cancel_bubble();
             },
             PauseIcon {
