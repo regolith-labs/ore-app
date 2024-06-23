1847efd56ee0cb4685552db072eac86416a7a03d
alex <aabinaei@gmail.com>
2024-06-23 11:23:38 -0700

    confirm markup

--- src/components/upgrade/mod.rs
+++ src/components/upgrade/mod.rs
@@ -1,24 +1,43 @@
+mod confirm;
 mod edit;
 
+use confirm::*;
 use edit::*;
 
 use dioxus::prelude::*;
 
 pub enum UpgradeStep {
     Edit,
+    Confirm,
 }
 
 #[component]
 pub fn Upgrade() -> Element {
+    let upgrade_step = use_signal(|| UpgradeStep::Edit);
     let amount_input = use_signal(|| "".to_string());
     let parsed_amount: u64 = match amount_input.read().parse::<f64>() {
         Ok(n) => (n * 10f64.powf(ore::TOKEN_DECIMALS_V1.into())) as u64,
         Err(_) => 0,
     };
-    rsx! {
-        UpgradeEdit {
-            amount_input: amount_input,
-            parsed_amount: parsed_amount
+
+    let e = match *upgrade_step.read() {
+        UpgradeStep::Edit => {
+            rsx! {
+                UpgradeEdit {
+                    upgrade_step: upgrade_step,
+                    amount_input: amount_input,
+                    parsed_amount: parsed_amount
+                }
+            }
         }
-    }
+        UpgradeStep::Confirm => {
+            rsx! {
+                UpgradeConfirm {
+                    upgrade_step: upgrade_step,
+                    amount: parsed_amount
+                }
+            }
+        }
+    };
+    e
 }
