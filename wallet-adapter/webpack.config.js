40151fd02d34a58dcb40a50d9aa01c1808826828
alex <aabinaei@gmail.com>
2024-06-25 15:57:21 -0700

    render v2 balance from react

--- wallet-adapter/webpack.config.js
+++ wallet-adapter/webpack.config.js
@@ -14,6 +14,13 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      vm: require.resolve('vm-browserify'),
+    },
+  },
   output: {
     filename: "wallet.js"
   }
