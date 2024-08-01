d4df37673f2f6d325109bab0e77e6b4bbc415dd7
Hardhat Chad <hardhatchad@gmail.com>
2024-08-01 15:10:56 -0700

    copy

--- src/components/download.rs
+++ src/components/download.rs
@@ -18,14 +18,14 @@ pub fn Download() -> Element {
                     "CLI"
                 }
                 p {
-                    "Use the ORE CLI to run a miner on any machine. "
-                    "To get started, ensure you have Rust and cargo installed. "
+                    "The ORE CLI provides a command line interface that can run on any computer or server. "
+                    "To get started, ensure you have Rust installed. "
                 }
                 CodeBlock {
                     text: "curl https://sh.rustup.rs -sSf | sh"
                 }
                 p {
-                    "Next, install the Solana CLI and create a Solana keypair if you haven't done so already. "
+                    "Next, install the Solana toolchain and create a Solana keypair if you haven't done so already. "
                 }
                 CodeBlock {
                     text: "sh -c \"$(curl -sSfL https://release.solana.com/v1.18.4/install)\"\nsolana-keygen new"
@@ -37,18 +37,18 @@ pub fn Download() -> Element {
                     text: "cargo install ore-cli"
                 }
                 p {
-                    "The ORE CLI uses your default Solana CLI config and identity. "
-                    "Ensure you have enough SOL topped up on this account to pay for transaction fees. "
+                    "The ORE CLI uses your existing Solana config and keypair by default. "
+                    "Ensure you have enough SOL topped up on your keypair to pay for transaction fees. "
                     "To begin mining, use the mine command."
                 }
                 CodeBlock {
-                    text: "ore \\\n    --rpc <RPC_URL> \\\n    --keypair <KEYPAIR_FILEPATH> \\\n    --priority-fee <MICROLAMPORTS> \\\n    mine \\\n    --threads <TRHEAD_COUNT>"
+                    text: "ore mine",
                 }
                 p {
-                    "To mine in detached mode, use nohup."
+                    "Use the -h flag to pull up a help menu anywhere in the CLI."
                 }
                 CodeBlock {
-                    text: "nohup ore mine > output.log 2>&1 &"
+                    text: "ore -h"
                 }
             }
         }
