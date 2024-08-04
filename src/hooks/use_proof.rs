757784c7163aeb081500e8c11ef54ef82b0ed3d4
Hardhat Chad <hardhatchad@gmail.com>
2024-08-04 11:40:17 -0700

    v1 claim page

--- src/hooks/use_proof.rs
+++ src/hooks/use_proof.rs
@@ -3,9 +3,12 @@ use ore_api::state::Proof;
 use ore_relayer_api::consts::ESCROW;
 use solana_client_wasm::solana_sdk::pubkey::Pubkey;
 
-use crate::gateway::{GatewayError, GatewayResult};
+use crate::gateway::{self, GatewayError, GatewayResult};
 
-use super::{use_escrow, use_gateway};
+use super::{
+    use_escrow, use_gateway,
+    use_wallet_adapter::{use_wallet_adapter, WalletAdapter},
+};
 
 pub fn use_proof() -> Resource<GatewayResult<Proof>> {
     let escrow = use_escrow();
@@ -28,5 +31,18 @@ pub fn use_user_proof(authority: Pubkey) -> Resource<GatewayResult<Proof>> {
     use_resource(move || {
         let gateway = gateway.clone();
         async move { gateway.get_proof(authority).await }
+    })
+}
+
+pub fn use_proof_v1() -> Resource<GatewayResult<ore_api_v1::state::Proof>> {
+    let wallet_adapter = use_wallet_adapter();
+    use_resource(move || async move {
+        match *wallet_adapter.read() {
+            WalletAdapter::Connected(pubkey) => {
+                let gateway = use_gateway();
+                gateway.get_proof_v1(pubkey).await
+            }
+            WalletAdapter::Disconnected => Err(GatewayError::AccountNotFound.into()),
+        }
     })
 }
