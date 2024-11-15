f02f9d8e59b76539b52aebc0fadc51c7467a46b6
Hardhat Chad <hardhatchad@gmail.com>
2024-11-14 17:18:51 -0800

    remove colors

--- src/components/table.rs
+++ src/components/table.rs
@@ -9,7 +9,7 @@ use crate::{
 pub fn Table(children: Element) -> Element {
     rsx! {
         Col {
-            class: "sm:mx-5 overflow-x-scroll bg-blue-500",
+            class: "sm:mx-5 overflow-x-scroll",
             {children}
         }
     }
@@ -19,16 +19,16 @@ pub fn Table(children: Element) -> Element {
 pub fn TableHeader(left: String, left_width: u64, right: Vec<String>) -> Element {
     rsx! {
         Row {
-            class: "h-8 sm:h-10 px-5 sm:px-3 justify-between font-medium text-xs sm:text-sm text-gray-700 bg-red-500",
+            class: "h-8 sm:h-10 w-full min-w-max px-5 sm:px-3 justify-between font-medium text-xs sm:text-sm text-gray-700",
             span {
-                class: "my-auto w-{left_width} shrink-0 bg-yellow-500",
+                class: "my-auto w-{left_width} shrink-0",
                 "{left}"
             }
             Row {
                 class: "text-right shrink-0",
                 for title in right {
                     span {
-                        class: "my-auto w-28 sm:w-40",
+                        class: "my-auto w-32 sm:w-40",
                         "{title}"
                     }
                 }
@@ -42,16 +42,16 @@ pub fn TableRowLink(to: Route, left: Element, left_width: u64, right: Vec<Elemen
     rsx! {
         Link {
             to: to,
-            class: "flex flex-row justify-between min-w-max w-full px-5 sm:px-3 py-4 transition sm:rounded-md hover:bg-controls-tertiary active:bg-controls-tertiaryHover hover:cursor-pointer bg-green-500",
+            class: "flex flex-row justify-between min-w-max w-full px-5 sm:px-3 py-4 transition sm:rounded-md hover:bg-controls-tertiary active:bg-controls-tertiaryHover hover:cursor-pointer",
             Row {
-                class: "w-{left_width} shrink-0 bg-purple-400",
+                class: "w-{left_width} shrink-0",
                 {left}
             }
             Row {
                 class: "text-right my-auto",
                 for r in right {
                     span {
-                        class: "flex w-28 sm:w-40 justify-end bg-purple-500",
+                        class: "flex w-32 sm:w-40 justify-end",
                         {r}
                     }
                 }
