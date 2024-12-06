64d1e98d223d4a327473baddc85c2079d8f24c3d
Hardhat Chad <hardhatchad@gmail.com>
2024-12-06 10:21:09 -0800

    update tokenomics page

--- src/components/ore_tokenomics.rs
+++ src/components/ore_tokenomics.rs
@@ -1,46 +1,25 @@
 use dioxus::prelude::*;
 
-// TODO Live supply
-// TODO Live circulating supply
-
-// TODO 1 ORE/min
-// TODO Emphasis on simple predictability and fairness.
-// TODO Linear supply inflation provides a baseline incentive for lending and spending
-// TODO Protection from exponential inflation
-// TODO Longterm sustainability in a way that deflationary currencies (such as BTC and ETH) do not.
-// TODO This supply rate will be the same 100 years from now as it is today
-// TODO Each generation will see approximately the same number of tokens mined (XYZ over the average 80 year human lifetime).
-// TODO 21 million new supply every ~40 years.
-// TODO Claims
-
 pub fn OreTokenomics() -> Element {
     rsx! {
         div {
             class: "flex flex-col gap-4 h-full font-hero max-w-3xl w-full mx-auto pb-20 leading-7",
             p {
                 class: "text-4xl font-bold",
-                "ORE tokenomics"
+                "ORE Tokenomics"
             }
             p {
-                "(Coming soon...)"
-            }
-            p {
-                "Here are the key points:"
                 li {
                     class: "ml-2",
-                    "ORE has a total maximum supply of 21 million tokens."
+                    "ORE has a total maximum supply cap of 5 million tokens."
                 }
                 li {
                     class: "ml-2",
-                    "On average, one new ORE token will be mined every minute."
+                    "Currently, ORE has an emissions rate of 1 ORE/min."
                 }
                 li {
                     class: "ml-2",
-                    "It will take ~40 years until the total ORE supply is fully mined."
-                }
-                li {
-                    class: "ml-2",
-                    "You can stake ORE to receive a multiplier on your mining rewards."
+                    "Emissions will reduce by 10% every ~12 months."
                 }
             }
         }
