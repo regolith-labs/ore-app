c19a054c7ac4afb139c44a47c3cfa9a119988a6e
alex <aabinaei@gmail.com>
2024-12-09 11:05:41 -0800

    wasm 2.99

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cargo install dioxus-cli
-cargo install -f wasm-bindgen-cli --version 0.2.97
+cargo install -f wasm-bindgen-cli --version 0.2.99
 rustup target add wasm32-unknown-unknown
 dx build --release
