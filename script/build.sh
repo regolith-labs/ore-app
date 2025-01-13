4fafe2d848e60233e2258ff18d8f30fa1a0761ff
Hardhat Chad <hardhatchad@gmail.com>
2025-01-13 09:48:38 -0800

    fix build script

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cargo install dioxus-cli --version 0.6
+cargo install dioxus-cli --version ^0.6
 cargo install -f wasm-bindgen-cli --version 0.2.99
 rustup target add wasm32-unknown-unknown
 dx build --release
