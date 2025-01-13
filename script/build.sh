5280154dffcd449d869edd5943aacaaa3eb0ca04
Hardhat Chad <hardhatchad@gmail.com>
2025-01-13 11:44:06 -0800

    locked install

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,4 @@
-cargo install dioxus-cli --version 0.5.7
+cargo install dioxus-cli --version 0.5.7 --locked
 cargo install -f wasm-bindgen-cli --version 0.2.93 
 rustup target add wasm32-unknown-unknown 
 dx build --release 
