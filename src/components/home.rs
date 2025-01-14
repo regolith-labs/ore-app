2d7d434a89b36d335218a9b14d052832b95f0319
Hardhat Chad <hardhatchad@gmail.com>
2025-01-13 23:47:15 -0800

    copy

--- src/components/home.rs
+++ src/components/home.rs
@@ -8,8 +8,8 @@ pub fn Home() -> Element {
             class: "flex flex-col gap-16 overflow-visible",
             Balance {}
             div {
-                class: "flex w-full px-4 py-2 bg-blue-500 text-white rounded-full",
-                "Mining on this page has been disabled. It will return soon with the rollout of the upcoming web app redesign."
+                class: "flex w-full px-4 py-2 bg-blue-500 text-white rounded",
+                "Web mining has been temporarily disabled. It will return soon with the rollout of the upcoming web app redesign."
             }
         }
     }
