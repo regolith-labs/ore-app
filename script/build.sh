18f264308829807ec1dc7264af4176ed8b2510b9
Hardhat Chad <hardhatchad@gmail.com>
2025-01-13 17:44:30 -0800

    cargo vendor

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cargo install dioxus-cli --version 0.6.0 --locked
-cargo install wasm-bindgen-cli --version 0.2.99 --force
+cargo install wasm-bindgen-cli --version 0.2.99 
 rustup target add wasm32-unknown-unknown
+cargo vendor -p wasm-bindgen
 dx build --release
