fb5e06458751e6be3633f5cb129b0dadc27a5265
Hardhat Chad <hardhatchad@gmail.com>
2025-01-13 18:03:23 -0800

    check versions

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cargo install dioxus-cli --version 0.6.0 --locked
 cargo install wasm-bindgen-cli --version 0.2.99 
+dx -V
+wasm-bindgen --V
 rustup target add wasm32-unknown-unknown
-cargo update -p wasm-bindgen
 dx build --release
