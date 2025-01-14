433717b91ff54e716231da1bb1707951657c158c
Hardhat Chad <hardhatchad@gmail.com>
2025-01-13 18:24:57 -0800

    verbose

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,7 @@ cargo install dioxus-cli --version 0.6.0 --locked
 cargo install wasm-bindgen-cli --version 0.2.99 
 dx -V
 wasm-bindgen -V
+which dx
 which wasm-bindgen
 rustup target add wasm32-unknown-unknown
-dx build --release
+dx build --release --verbose
