a522ead2ba44802566df29227bd6d766929e3fe8
Hardhat Chad <hardhatchad@gmail.com>
2025-02-02 11:55:12 -0800

    cleanup

--- src/gateway/ore.rs
+++ src/gateway/ore.rs
@@ -1,7 +1,3 @@
-use ore_api::{
-    consts::CONFIG_ADDRESS,
-    state::{proof_pda, Config, Proof},
-};
 use ore_boost_api::state::{Boost, Stake};
 use solana_sdk::pubkey::Pubkey;
 use steel::AccountDeserialize;
@@ -9,29 +5,11 @@ use steel::AccountDeserialize;
 use super::{GatewayError, GatewayResult, Rpc};
 
 pub trait OreGateway {
-    async fn get_config(&self) -> GatewayResult<Config>;
-    async fn get_proof(&self, authority: Pubkey) -> GatewayResult<Proof>;
     async fn get_boost(&self, mint: Pubkey) -> GatewayResult<Boost>;
     async fn get_stake(&self, address: Pubkey) -> GatewayResult<Stake>;
 }
 
 impl<R: Rpc> OreGateway for R {
-    async fn get_config(&self) -> GatewayResult<Config> {
-        let data = self
-            .get_account_data(&CONFIG_ADDRESS)
-            .await
-            .map_err(GatewayError::from)?;
-        Ok(*Config::try_from_bytes(&data)?)
-    }
-
-    async fn get_proof(&self, authority: Pubkey) -> GatewayResult<Proof> {
-        let data = self
-            .get_account_data(&proof_pda(authority).0)
-            .await
-            .map_err(GatewayError::from)?;
-        Ok(*Proof::try_from_bytes(&data)?)
-    }
-
     async fn get_boost(&self, address: Pubkey) -> GatewayResult<Boost> {
         let data = self
             .get_account_data(&address)
