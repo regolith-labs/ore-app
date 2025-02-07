ae64d4890d7e2e161f634abb55ce56798f4cfc68
Marco <tmiwia@gmail.com>
2025-02-06 20:41:43 -0500

    Comment clean up

--- src/hooks/transaction_builders/use_miner_claim_transaction.rs
+++ src/hooks/transaction_builders/use_miner_claim_transaction.rs
@@ -22,16 +22,21 @@ pub fn use_miner_claim_transaction(
         };        
     
         // Destructure member_on_chain from Resouce<Result<Member>>
+        // let data = &*member_on_chain.value().read_unchecked();
         let data = &*member_on_chain.value().read_unchecked();
-        log::info!("user_miner data: {:?}", data);
+        // let Some(Ok (data)) = member_on_chain.cloned() else {return Err(GatewayError::Unknown)};
+        
+        // log::info!("user_miner data: {:?}", data);
         
         // Get member data from Member
         let member_data = match data {
             Some(Ok(member)) => member, // Extract the Member if successful
             Some(Err(err)) => return Err(GatewayError::Unknown), // Handle the Err case
             None => return Err(GatewayError::Unknown), // Handle the None case
-        };        
-            
+        };  
+
+        log::info!("Miner data: {:?}", member_data);
+
         // Check if miner has no balance to claim
         if member_data.balance <= 0 {
             return Err(GatewayError::Unknown);
@@ -44,9 +49,10 @@ pub fn use_miner_claim_transaction(
 
         // Get the associated token address for miner
         let ata_address = get_associated_token_address(&authority, &ore_api::consts::MINT_ADDRESS);
+        // log::info!("Associated token address: {:?}", ata_address);
 
         // Check if the associated token account exists
-        if gateway.rpc.get_token_account(&authority).await.is_err() {            
+        if gateway.rpc.get_token_account(&ata_address).await.is_err() {            
             let create_ata_ix = spl_associated_token_account::instruction::create_associated_token_account(
                 &authority,
                 &authority,
@@ -60,13 +66,16 @@ pub fn use_miner_claim_transaction(
 
         // Use the ata_address directly since it should now exist
         let beneficiary = ata_address; 
+        // log::info!("Beneficiary address: {:?}", beneficiary);
 
         // Add claim transaction
         ixs.push(ore_pool_api::sdk::claim(authority, beneficiary, member_data.pool, member_data.balance));
-
+        // log instructions vector
+        // log::info!("Instructions: {:?}", ixs);
        
         // Build transaction
-        let tx = Transaction::new_with_payer(&ixs, Some(&authority)).into();
+        let tx: VersionedTransaction = Transaction::new_with_payer(&ixs, Some(&authority)).into();
+        // log::info!("Transaction: {:?}", tx);
         Ok(tx)
     })
 }
