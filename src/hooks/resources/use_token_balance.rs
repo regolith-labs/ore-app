09f55a9410ffe11821ca67fd4d4b7e96205003c4
Marco <tmiwia@gmail.com>
2025-02-14 14:58:46 -0500

    WIP creating cache for token balances

--- src/hooks/resources/use_token_balance.rs
+++ src/hooks/resources/use_token_balance.rs
@@ -1,11 +1,122 @@
 use dioxus::prelude::*;
 use solana_sdk::{native_token::lamports_to_sol, pubkey::Pubkey};
+use std::collections::HashMap;
 
-use crate::{config::Token, gateway::{
+use crate::{config::{Token, LISTED_TOKENS}, gateway::{
     spl::SplGateway, GatewayError, GatewayResult, Rpc, UiTokenAmount
 }, utils::LiquidityPair};
 
 use crate::hooks::{use_gateway, use_wallet, Wallet};
+
+// Create token balances resource
+pub(crate) fn use_token_balance_provider() {
+    let mut token_balances = HashMap::new();
+
+    for pubkey in LISTED_TOKENS.keys() {
+        let token_mint = *pubkey;
+        token_balances.insert(token_mint, use_token_balance_resource(token_mint));
+    }
+
+    use_context_provider(|| token_balances);
+}
+
+fn use_token_balance_resource(mint: Pubkey) -> Resource<GatewayResult<UiTokenAmount>> {
+    let wallet_status = use_wallet();
+    use_resource(move || async move {
+        match *wallet_status.read() {
+            Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
+            Wallet::Connected(pubkey) => get_token_balance(pubkey, mint).await,
+        }
+    })
+}
+
+
+//TODO: START HERE
+// get resource from hahsmap conteext, return the resource if it exisstss
+pub fn use_token_balance(mint: Pubkey) -> Resource<GatewayResult<UiTokenAmount>> {
+    let token_balances: HashMap<Pubkey, Resource<GatewayResult<UiTokenAmount>>> = use_context();
+    if let Some(balance) = token_balances.get(&mint) {
+        *balance
+    } else {
+        use_token_balance_resource(mint)
+    }
+    
+    // BEFORE
+    // let wallet_status = use_wallet();
+    // use_resource(move || async move {
+    //     match *wallet_status.read() {
+    //         Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
+    //         Wallet::Connected(pubkey) => get_token_balance(pubkey, mint).await,
+    //     }
+    // })
+}
+// IGNOORE THIS FOR NOW
+pub fn use_token_balance_for_token(token: Signal<Option<Token>>) -> Resource<GatewayResult<UiTokenAmount>> {
+    let wallet_status = use_wallet();
+    use_resource(move || async move {
+        let Some(token) = token.read().clone() else {
+            return Err(GatewayError::Unknown.into());
+        };
+        match *wallet_status.read() {
+            Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
+            Wallet::Connected(pubkey) => get_token_balance(pubkey, token.mint).await,
+        }
+    })
+}
+
+async fn get_token_balance(pubkey: Pubkey, mint: Pubkey) -> GatewayResult<UiTokenAmount> {
+    if mint == Token::sol().mint {        
+        use_gateway()
+            .rpc
+            .get_balance(&pubkey)
+            .await
+            .map(|lamports| {
+                let sol = lamports_to_sol(lamports);
+                UiTokenAmount {
+                    ui_amount: Some(sol),
+                    decimals: 8,
+                    amount: format!("{}", lamports).to_owned(),
+                    ui_amount_string: format!("{}", sol).to_owned(),
+                }
+            })
+            .map_err(GatewayError::from)
+    // All other token balances
+    } else {
+        use_gateway()
+            .rpc
+            .get_token_balance(&pubkey, &mint)
+            .await
+            .map_err(GatewayError::from)
+    }
+}
+
+pub fn use_token_balances_for_liquidity_pair(liquidity_pair: Resource<GatewayResult<LiquidityPair>>) -> (Resource<GatewayResult<UiTokenAmount>>, Resource<GatewayResult<UiTokenAmount>>) {
+    let wallet = use_wallet();
+
+    let token_a_balance = use_resource(move || async move {
+        if let Some(Ok(liquidity_pair)) = liquidity_pair.read().as_ref() {
+            match *wallet.read() {
+                Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
+                Wallet::Connected(authority) => get_token_balance(authority, liquidity_pair.token_a.mint).await,
+            }
+        } else {
+            Err(GatewayError::Unknown)
+        }
+    });
+
+    let token_b_balance = use_resource(move || async move {
+        if let Some(Ok(liquidity_pair)) = liquidity_pair.read().as_ref() {
+            match *wallet.read() {
+                Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
+                Wallet::Connected(authority) => get_token_balance(authority, liquidity_pair.token_b.mint).await,
+            }
+        } else {
+            Err(GatewayError::Unknown)
+        }
+    });
+
+    (token_a_balance, token_b_balance)
+}
 
 pub fn _use_sol_balance() -> Resource<GatewayResult<u64>> {
     let wallet = use_wallet();
@@ -39,92 +150,4 @@ pub fn _use_ore_supply() -> Resource<GatewayResult<UiTokenAmount>> {
             .await
             .map_err(GatewayError::from)
     })
-}
-
-//TODO: START HERE
-// get resoruce from hahsmap conteext, return the resource if it exisstss
-pub fn use_token_balance(mint: Pubkey) -> Resource<GatewayResult<UiTokenAmount>> {
-    let wallet_status = use_wallet();
-    use_resource(move || async move {
-        match *wallet_status.read() {
-            Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
-            Wallet::Connected(pubkey) => get_token_balance(pubkey, mint).await,
-        }
-    })
-}
-// IGNOORE THIS FOR NOW
-pub fn use_token_balance_for_token(token: Signal<Option<Token>>) -> Resource<GatewayResult<UiTokenAmount>> {
-    let wallet_status = use_wallet();
-    use_resource(move || async move {
-        let Some(token) = token.read().clone() else {
-            return Err(GatewayError::Unknown.into());
-        };
-        match *wallet_status.read() {
-            Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
-            Wallet::Connected(pubkey) => get_token_balance(pubkey, token.mint).await,
-        }
-    })
-}
-
-/*
-use_resoource() {
-
-
-}
-*/
-async fn get_token_balance(pubkey: Pubkey, mint: Pubkey) -> GatewayResult<UiTokenAmount> {
-    // todo: this is what we want to cache this whole block (if else()
-    // mint -> key
-    // value -> GatewayResult<UiTokenAmount>
-    if mint == Token::sol().mint {        
-        use_gateway()
-            .rpc
-            .get_balance(&pubkey)
-            .await
-            .map(|lamports| {
-                let sol = lamports_to_sol(lamports);
-                UiTokenAmount {
-                    ui_amount: Some(sol),
-                    decimals: 8,
-                    amount: format!("{}", lamports).to_owned(),
-                    ui_amount_string: format!("{}", sol).to_owned(),
-                }
-            })
-            .map_err(GatewayError::from)
-    } else {
-        use_gateway()
-            .rpc
-            .get_token_balance(&pubkey, &mint)
-            .await
-            .map_err(GatewayError::from)
-    }
-}
-
-
-pub fn use_token_balances_for_liquidity_pair(liquidity_pair: Resource<GatewayResult<LiquidityPair>>) -> (Resource<GatewayResult<UiTokenAmount>>, Resource<GatewayResult<UiTokenAmount>>) {
-    let wallet = use_wallet();
-
-    let token_a_balance = use_resource(move || async move {
-        if let Some(Ok(liquidity_pair)) = liquidity_pair.read().as_ref() {
-            match *wallet.read() {
-                Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
-                Wallet::Connected(authority) => get_token_balance(authority, liquidity_pair.token_a.mint).await,
-            }
-        } else {
-            Err(GatewayError::Unknown)
-        }
-    });
-
-    let token_b_balance = use_resource(move || async move {
-        if let Some(Ok(liquidity_pair)) = liquidity_pair.read().as_ref() {
-            match *wallet.read() {
-                Wallet::Disconnected => Err(GatewayError::AccountNotFound.into()),
-                Wallet::Connected(authority) => get_token_balance(authority, liquidity_pair.token_b.mint).await,
-            }
-        } else {
-            Err(GatewayError::Unknown)
-        }
-    });
-
-    (token_a_balance, token_b_balance)
 }
