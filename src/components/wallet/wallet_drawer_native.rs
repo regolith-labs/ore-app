5617073f644aff66c3e85945bcc60d7e208f9bb0
alex <aabinaei@gmail.com>
2025-02-20 12:31:43 -0800

    wallet-drawer native

--- src/components/wallet/wallet_drawer_native.rs
+++ src/components/wallet/wallet_drawer_native.rs
@@ -2,13 +2,10 @@ use dioxus::prelude::*;
 
 use crate::hooks::{use_wallet, Wallet};
 
-use crate::components::*;
-
 #[component]
 pub fn WalletDrawer(on_close: EventHandler<MouseEvent>) -> Element {
     let wallet = use_wallet();
-    let tab = use_signal(|| WalletTab::Tokens);
-    let mut pubkey = use_signal(|| "no pubkey".to_string());
+    let mut pubkey = use_signal(|| "missing pubkey".to_string());
     use_effect(move || {
         if let Wallet::Connected(pk) = *wallet.read() {
             pubkey.set(pk.to_string());
@@ -17,20 +14,9 @@ pub fn WalletDrawer(on_close: EventHandler<MouseEvent>) -> Element {
     rsx! {
         div {
             class: "flex flex-col gap-8 h-full sm:w-96 w-screen elevated elevated-border text-white py-8 z-50",
-            // "TODO: Wallet address + copy button"
             onclick: move |e| e.stop_propagation(),
+            // "TODO: Wallet address + copy button"
             div { "{pubkey}" }
-            Col {
-                WalletTabs { tab },
-                match *tab.read() {
-                    WalletTab::Tokens => rsx! {
-                        TokenTable { on_close }
-                    },
-                    WalletTab::Liquidity => rsx! {
-                        LiquidityTable { on_close }
-                    }
-                }
-            }
         }
     }
 }
