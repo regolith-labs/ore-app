a503e379b6df046440e1a9476bf94634dc9d842f
alex <aabinaei@gmail.com>
2025-02-20 12:17:44 -0800

    single spl gateway impl

--- src/gateway/spl.rs
+++ src/gateway/spl.rs
@@ -1,10 +1,6 @@
 use ore_api::consts::MINT_ADDRESS;
 use solana_sdk::{program_pack::Pack, pubkey::Pubkey};
 
-#[cfg(not(feature = "web"))]
-use super::NativeRpc;
-#[cfg(feature = "web")]
-use super::WebRpc;
 use super::{GatewayError, GatewayResult, Rpc, UiTokenAmount};
 use crate::solana::{
     spl_associated_token_account::get_associated_token_address, spl_token::state::Mint,
@@ -22,29 +18,7 @@ pub trait SplGateway {
     }
 }
 
-#[cfg(feature = "web")]
-impl SplGateway for WebRpc {
-    async fn get_token_balance(
-        &self,
-        owner: &Pubkey,
-        mint: &Pubkey,
-    ) -> GatewayResult<UiTokenAmount> {
-        let ata_address = get_associated_token_address(owner, &mint);
-        let Some(token_account) = self.get_token_account(&ata_address).await? else {
-            return Err(GatewayError::AccountNotFound.into());
-        };
-        Ok(token_account)
-    }
-
-    async fn get_mint(&self, mint: &Pubkey) -> GatewayResult<Mint> {
-        let mint = self.get_account_data(mint).await?;
-        let mint = Mint::unpack(&mint.as_slice())?;
-        Ok(mint)
-    }
-}
-
-#[cfg(not(feature = "web"))]
-impl SplGateway for NativeRpc {
+impl<R: Rpc> SplGateway for R {
     async fn get_token_balance(
         &self,
         owner: &Pubkey,
