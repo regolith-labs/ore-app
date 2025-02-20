d23c45876d3f8e4618afce6f61a9bdbe5dd20619
Hardhat Chad <hardhatchad@gmail.com>
2025-02-20 11:57:02 -0800

    landing

--- src/pages/landing.rs
+++ src/pages/landing.rs
@@ -10,6 +10,7 @@ pub fn Landing() -> Element {
         Liquidity {}
         Stats {}
         Community {}
+        Faq {}
         Footer {}
     }
 }
@@ -83,7 +84,7 @@ fn Mining() -> Element {
                 class: "relative w-full h-min mx-auto max-w-7xl justify-start",
                 SectionCopy {
                     class: "bg-linear-to-r from-transparent via-black to-transparent from-10% via-50% to-90%",
-                    tip: "Hard money",
+                    tip: "Fair launch",
                     title: "Proof of work.",
                     subtitle: "On Solana.", 
                     detail: "Start mining crypto in just one click."
@@ -172,7 +173,7 @@ fn Liquidity() -> Element {
             Col {
                 class: "w-full h-min mx-auto max-w-7xl justify-start",
                 SectionCopy {
-                    tip: "Defi native",
+                    tip: "Defi",
                     title: "Deep liquidity.",
                     subtitle: "Native yield.",
                     detail: "Stake your crypto and earn yield."
@@ -188,7 +189,7 @@ fn Community() -> Element {
         Col {
             class: "relative w-full h-full mx-auto max-w-7xl min-h-screen pt-16 px-4",
             SectionCopy {
-                tip: "Social network",
+                tip: "Social",
                 title: "Join the community.",
                 detail: "Discover why people around the world love ORE."
             }
@@ -199,7 +200,7 @@ fn Community() -> Element {
 fn Testimonials() -> Element {
     rsx! {
         Col {
-            class: "w-full h-min mx-auto max-w-7xl justify-start",
+            class: "w-full h-min mx-auto max-w-7xl justify-start mt-8",
             Row {
                 class: "w-full gap-4",
                 Col {
@@ -211,10 +212,10 @@ fn Testimonials() -> Element {
                         link: "https://x.com/mattytay/status/1870887900663169059"
                     }
                     Testimonial {
-                        image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Felix",
-                        name: "John Doe",
-                        quote: "This is an amazing project! The technology behind it is truly revolutionary and I'm excited to see where it goes.",
-                        link: "https://x.com/"
+                        image: "https://pbs.twimg.com/profile_images/1857467519042232321/GLvZxG-T_400x400.jpg",
+                        name: "network state enjoyooor",
+                        quote: "ORE becoming more gamified with every new update . This is what mining evolution looks like.",
+                        link: "https://x.com/lowercaseben/status/1878117108287943112"
                     }
                     Testimonial {
                         image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Felix",
@@ -329,13 +330,12 @@ fn Stats() -> Element {
         Col {
             class: "w-full h-min text-elements-highEmphasis px-4",
             Col {
-                class: "md:flex-row md:gap-24 relative w-full py-32 px-4 mx-auto max-w-7xl border-t border-b border-elements-midEmphasis",
+                class: "md:flex-row md:gap-24 relative w-full py-16 md:py-32 px-4 mx-auto max-w-7xl border-t border-b border-elements-midEmphasis",
                 gap: 16,
-
                 Col {
                     class: "md:ml-auto",
                     span {
-                        class: "text-3xl md:text-4xl lg:text-5xl font-extended font-bold text-center text-elements-highEmphasis",
+                        class: "text-4xl lg:text-5xl font-extended font-bold text-center text-elements-highEmphasis",
                         if let Some(Ok(holders)) = holders.cloned() {
                             "{format_abbreviated_number(holders as f64)}"
                         } else {
@@ -349,9 +349,9 @@ fn Stats() -> Element {
                 }
 
                 Col {
-                    class: "",
+                    class: "text-left",
                     span {
-                        class: "text-3xl md:text-4xl lg:text-5xl font-extended font-bold text-center text-elements-highEmphasis",
+                        class: "text-4xl lg:text-5xl font-extended font-bold text-center text-elements-highEmphasis",
                         "5,000,000"
                     }
                     span {
@@ -363,7 +363,7 @@ fn Stats() -> Element {
                 Col {
                     class: "md:mr-auto",
                     span {
-                        class: "text-3xl md:text-4xl lg:text-5xl font-extended font-bold text-center text-elements-highEmphasis",
+                        class: "text-4xl lg:text-5xl font-extended font-bold text-center text-elements-highEmphasis",
                         if let Some(Ok(market_cap)) = market_cap.cloned() {
                             "${format_abbreviated_number(market_cap)}"
                         } else {
@@ -380,16 +380,91 @@ fn Stats() -> Element {
     }
 }
 
-#[component]
-fn SectionCopy(class: Option<String>, tip: Option<String>, title: String, subtitle: Option<String>, detail: Option<String>) -> Element {
-    let class = class.unwrap_or_default();
+fn Faq() -> Element {
     rsx! {
         Col {
-            class: "py-8 font-wide font-bold text-4xl md:text-5xl lg:text-6xl text-center text-elements-highEmphasis selection:bg-elements-highEmphasis selection:text-black px-4 {class}",
+            class: "md:flex-row w-full h-min mx-auto max-w-7xl justify-start mt-16 px-4",
+            SectionCopy {
+                class: "text-left md:min-w-sm lg:min-w-md",
+                align: Align::Left,
+                tip: "Support",
+                title: "FAQ",
+                detail: "Commonly asked questions."
+            }
+            Col {
+                class: "w-full h-min justify-start md:mt-16",
+                FaqItem {
+                    question: "What is ORE?",
+                    answer: "ORE is a new \"digital gold\" primitive for decentralized finance. It is a crypto commodity mineable via proof-of-work on the Solana blockchain."
+                }
+                FaqItem {
+                    question: "Why should I care?",
+                    answer: "ORE is a hard money token on the Solana blockchain."
+                }
+                FaqItem {
+                    question: "How does mining work?",
+                    answer: "ORE is a hard money token on the Solana blockchain."
+                }
+                FaqItem {
+                    question: "How does staking work?",
+                    answer: "ORE is a hard money token on the Solana blockchain."
+                }
+                FaqItem {
+                    question: "Where does the yield come from?",
+                    answer: "ORE is a hard money token on the Solana blockchain."
+                }
+            }
+        }
+    }
+}
+#[component]
+fn FaqItem(question: String, answer: String) -> Element {
+    let mut is_open = use_signal(|| false);
+    let rotation = if is_open.cloned() { "rotate-45" } else { "rotate-0" };
+    rsx! {
+        button {
+            class: "flex flex-col py-8 px-8 cursor-pointer transition-all duration-300 ease-in-out rounded hover:bg-elements-midEmphasis/10",
+            onclick: move |_| is_open.set(!is_open.cloned()),
+            Row {
+                class: "justify-between font-wide text-left font-bold text-2xl text-elements-highEmphasis",
+                gap: 8,
+                "{question}"
+                PlusIcon {
+                    class: "w-4 h-4 my-auto transition-transform duration-300 ease-in-out text-elements-lowEmphasis {rotation}"
+                }
+            }
+            div {
+                class: "overflow-hidden transition-all duration-300 ease-in-out",
+                style: if is_open.cloned() { "max-height: 200px; opacity: 1;" } else { "max-height: 0px; opacity: 0;" },
+                p {
+                    class: "text-elements-midEmphasis mt-4 text-lg text-left",
+                    "{answer}"
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum Align {
+    Left,
+    Center,
+}
+
+#[component]
+fn SectionCopy(class: Option<String>, align: Option<Align>, tip: Option<String>, title: String, subtitle: Option<String>, detail: Option<String>) -> Element {
+    let class = class.unwrap_or_default();
+    let (text_align, text_margin) = match align.unwrap_or(Align::Center) {
+        Align::Left => ("text-left", "mr-auto"),
+        Align::Center => ("text-center", "mx-auto"),
+    };
+    rsx! {
+        Col {
+            class: "py-8 font-wide font-bold text-4xl md:text-5xl lg:text-6xl text-elements-highEmphasis selection:bg-elements-highEmphasis selection:text-black px-4 {class} {text_align}",
             gap: 2,
             if let Some(tip) = tip {
                 span {
-                    class: "z-30 border-2 border-elements-gold text-elements-gold rounded-full px-4 py-2 text-xs font-semibold mx-auto mb-4",
+                    class: "z-30 border-2 border-elements-gold text-elements-gold rounded-full w-min px-4 py-2 text-xs font-semibold mb-4 text-nowrap {text_margin}",
                     "{tip}"
                 }
             }
@@ -405,7 +480,7 @@ fn SectionCopy(class: Option<String>, tip: Option<String>, title: String, subtit
             }
             if let Some(detail) = detail {
                 span {
-                    class: "md:mb-auto mt-4 z-10 text-elements-midEmphasis font-wide font-medium text-lg sm:text-xl md:text-2xl mx-auto",
+                    class: "md:mb-auto mt-4 z-10 text-elements-midEmphasis font-wide font-medium text-lg sm:text-xl md:text-2xl {text_margin}",
                     "{detail}"
                 }
             }
@@ -416,13 +491,14 @@ fn SectionCopy(class: Option<String>, tip: Option<String>, title: String, subtit
 fn Footer() -> Element {
     rsx! {
         Col {
-            class: "w-screen h-full text-elements-highEmphasis pt-8 px-4",
+            class: "w-screen h-full text-elements-highEmphasis pt-32 px-4",
             Row {
                 class: "w-full h-min mx-auto max-w-7xl justify-end pb-4",
                 SocialLinks {}
             }
             Col {
                 class: "w-full h-min mx-auto max-w-7xl justify-between border-t border-elements-midEmphasis pt-4",
+                // class: "w-full h-min mx-auto max-w-7xl justify-between pt-4",
                 gap: 16,
                 span {
                     class: "text-elements-lowEmphasis text-sm font-medium ml-auto mr-4",
