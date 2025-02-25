ac85d49ecb68a5ebcd1ef047d38b2e24328f203a
big time tap in <bigtimetapin@gmail.com>
2025-02-24 18:51:22 -0800

    Native Miner (#54)
    * native miner
    
    * fault-tolerant pool registration
    
    * Code TLC
    
    * member-record.resource.restart; bug
    
    * print difficulty
    
    * member-balance resource
    
    * only submit global best solutions
    
    * Code TLC
    
    * Code TLC
    
    * Code TLC
    
    * single-core

--- src/hooks/miner/use_miner_native.rs
+++ src/hooks/miner/use_miner_native.rs
@@ -1,11 +1,190 @@
 use dioxus::prelude::*;
 
+use anyhow::Result;
+use drillx::Solution;
+use futures::StreamExt;
 use ore_miner_types::{InputMessage, OutputMessage};
 
-/// two way channel between us and miner (web worker)
+/// two way channel between us and miner
 pub fn use_miner_provider() {
     // from miner receiver
-    let _from_miner = use_context_provider(|| Signal::new(OutputMessage::Init));
+    let mut from_miner = use_context_provider(|| Signal::new(OutputMessage::Init));
     // to miner sender
-    let _to_miner: Coroutine<InputMessage> = use_coroutine(move |mut _rx| async move {});
+    let _to_miner: Coroutine<InputMessage> = use_coroutine(
+        move |mut rx: dioxus::prelude::UnboundedReceiver<InputMessage>| async move {
+            // build continuous solutions channel
+            let (sender, mut receiver) = tokio::sync::mpsc::unbounded_channel::<OutputMessage>();
+            // poll for messages from controller
+            while let Some(msg) = rx.next().await {
+                log::info!("to worker: {:?}", msg);
+                // spawn miner
+                let sender = sender.clone();
+                tokio::spawn(async move {
+                    let cores = 1;
+                    let device_id = 0;
+                    let challenge = msg.challenge.challenge;
+                    match nonce_indices(&msg.member, &msg.challenge, cores, device_id) {
+                        Ok(nonce_indices) => {
+                            if let Err(err) = find_hash_par(
+                                &challenge.challenge,
+                                challenge.lash_hash_at,
+                                nonce_indices.as_slice(),
+                                msg.cutoff_time as u64,
+                                challenge.min_difficulty as u32,
+                                cores,
+                                &sender,
+                            )
+                            .await
+                            {
+                                log::error!("{:?}", err);
+                            }
+                        }
+                        Err(err) => {
+                            log::error!("{:?}", err);
+                        }
+                    }
+                });
+                // listen for solutions from miner
+                let mut best_difficulty = 0;
+                while let Some(msg) = receiver.recv().await {
+                    // submit best solutions
+                    if let OutputMessage::Solution(solution) = msg {
+                        let difficulty = solution.to_hash().difficulty();
+                        // submit
+                        if difficulty.gt(&best_difficulty) {
+                            from_miner.set(msg);
+                            best_difficulty = difficulty;
+                            log::info!("found new best difficulty: {}", best_difficulty);
+                        }
+                    }
+                    // exit if expired
+                    if let OutputMessage::Expired(_) = msg {
+                        log::info!("expired");
+                        from_miner.set(msg);
+                        break;
+                    }
+                }
+            }
+        },
+    );
+}
+
+async fn find_hash_par(
+    challenge: &[u8; 32],
+    last_hash_at: i64,
+    nonce_indices: &[u64],
+    cutoff_time: u64,
+    min_difficulty: u32,
+    cores: u8,
+    solutions_channel: &tokio::sync::mpsc::UnboundedSender<OutputMessage>,
+) -> Result<()> {
+    // get cores
+    let core_ids = core_affinity::get_core_ids().ok_or(anyhow::anyhow!("failed to query cores"))?;
+    let core_ids = core_ids.into_iter().filter(|id| id.id < (cores as usize));
+    // distribute
+    for core_id in core_ids {
+        let challenge = *challenge;
+        std::thread::spawn({
+            // init drillx solver
+            let mut memory = drillx::equix::SolverMemory::new();
+            let solutions_channel = solutions_channel.clone();
+            let nonce = nonce_indices[core_id.id];
+            move || {
+                // pin to core
+                let _ = core_affinity::set_for_current(core_id);
+                // start hashing
+                let timer = std::time::Instant::now();
+                let mut nonce = nonce;
+                let mut best_difficulty = 0;
+                loop {
+                    // get hashes
+                    let hxs =
+                        drillx::hashes_with_memory(&mut memory, &challenge, &nonce.to_le_bytes());
+                    // look for best difficulty score in all hashes
+                    for hx in hxs {
+                        let difficulty = hx.difficulty();
+                        if difficulty.gt(&best_difficulty) {
+                            best_difficulty = difficulty;
+                            // continously submit best solution to pool
+                            if difficulty.ge(&min_difficulty) {
+                                let diggest = hx.d;
+                                let nonce = nonce.to_le_bytes();
+                                log::info!("/////////////////////////////////////");
+                                log::info!("difficulty: {}", difficulty);
+                                let solution = Solution {
+                                    d: diggest,
+                                    n: nonce,
+                                };
+                                let solution = OutputMessage::Solution(solution);
+                                if let Err(err) = solutions_channel.send(solution) {
+                                    log::error!("{:?}", err);
+                                }
+                            }
+                        }
+                    }
+                    // exit if time has elapsed
+                    if nonce % 10 == 0 {
+                        if timer.elapsed().as_secs().ge(&cutoff_time) {
+                            // send expiration message
+                            if core_id.id == 0 {
+                                let expired = OutputMessage::Expired(last_hash_at);
+                                if let Err(err) = solutions_channel.send(expired) {
+                                    log::error!("{:?}", err);
+                                }
+                            }
+                            break;
+                        } else if core_id.id == 0 {
+                            log::info!(
+                                "Mining... Time remaining: {} {}",
+                                format_duration(
+                                    cutoff_time.saturating_sub(timer.elapsed().as_secs()) as u32
+                                ),
+                                nonce,
+                            );
+                        }
+                    }
+                    // increment nonce
+                    nonce += 1;
+                }
+            }
+        });
+    }
+    Ok(())
+}
+
+fn nonce_indices(
+    member: &ore_pool_types::Member,
+    challenge: &ore_pool_types::MemberChallenge,
+    cores: u8,
+    device_id: u8,
+) -> Result<Vec<u64>> {
+    // build nonce indices
+    let cores = cores as u64;
+    let num_total_members = challenge.num_total_members.max(1);
+    let member_search_space_size = u64::MAX.saturating_div(num_total_members);
+    let device_search_space_size =
+        member_search_space_size.saturating_div(challenge.num_devices as u64);
+    // check device id doesn't go beyond pool limit
+    if device_id > challenge.num_devices {
+        return Err(anyhow::anyhow!("too many devices"));
+    }
+    // calculate bounds on nonce space
+    let left_bound = member_search_space_size.saturating_mul(member.id as u64)
+        + (device_id as u64).saturating_mul(device_search_space_size);
+    log::info!("left bound: {}", left_bound);
+    // split nonce-device space for muliple cores
+    let range_per_core = device_search_space_size.saturating_div(cores);
+    let mut nonce_indices = Vec::with_capacity(cores as usize);
+    for n in 0..(cores) {
+        let index = left_bound + n * range_per_core;
+        nonce_indices.push(index);
+    }
+    log::info!("nonces: {:?}", nonce_indices);
+    Ok(nonce_indices)
+}
+
+fn format_duration(seconds: u32) -> String {
+    let minutes = seconds / 60;
+    let remaining_seconds = seconds % 60;
+    format!("{:02}:{:02}", minutes, remaining_seconds)
 }
--- src/pages/mine.rs
+++ src/pages/mine.rs
@@ -5,7 +5,7 @@ use crate::{
     components::*,
     gateway::pool::PoolGateway,
     hooks::{
-        on_transaction_done, use_gateway, use_member, use_member_record,
+        on_transaction_done, use_gateway, use_member, use_member_record, use_member_record_balance,
         use_miner_claim_transaction, use_miner_is_active, use_miner_status,
         use_pool_register_transaction, use_pool_url, use_wallet, MinerStatus, Wallet,
     },
@@ -75,7 +75,7 @@ fn StopStartButton() -> Element {
     let register_tx = use_pool_register_transaction();
     let is_active = use_miner_is_active();
 
-    let mut f = use_future(move || async move {
+    let mut register_with_pool_server = use_future(move || async move {
         let Wallet::Connected(authority) = *wallet.read() else {
             return;
         };
@@ -92,9 +92,25 @@ fn StopStartButton() -> Element {
         }
     });
 
+    // on successful registration signature (onchain),
+    // restart the pool server registration
+    //
+    // this is the happy path,
+    // first the onchain registration signature lands
+    // and then we submit for registration with the offchain pool server
     on_transaction_done(move |_sig| {
         if miner_status.cloned() == MinerStatus::Registering {
-            f.restart();
+            register_with_pool_server.restart();
+        }
+    });
+    // submit pool server registration
+    //
+    // this is the recovery path,
+    // where the onchain registration landed
+    // but the server registration failed or hasn't been submitted yet
+    use_memo(move || {
+        if miner_status.cloned() == MinerStatus::Registering {
+            register_with_pool_server.restart();
         }
     });
 
@@ -109,12 +125,24 @@ fn StopStartButton() -> Element {
             class: "flex flex-row gap-2 my-auto px-8 h-12 rounded-full {controls_class}",
             disabled: matches!(*wallet.read(), Wallet::Disconnected),
             onclick: move |_| {
+                // set to stopped (miner is active)
                 if *is_active.read() {
                     miner_status.set(MinerStatus::Stopped);
                 } else {
+                    // stopped
+                    // if already registered in pool fetch next challenge
+                    // or else try registering (both onchain and then with the pool server)
                     if let Some(Ok(_member)) = member.cloned() {
-                        miner_status.set(MinerStatus::FetchingChallenge);
+                        // already registered on chain, check if registered with pool server
+                        if let Some(Ok(_member_db)) = member_record.cloned() {
+                            // registered with the pool server too, fetch challenge
+                            miner_status.set(MinerStatus::FetchingChallenge);
+                        } else {
+                            // only registered onchain, submit registration to pool server
+                            miner_status.set(MinerStatus::Registering);
+                        }
                     } else if let Some(Ok(tx)) = register_tx.cloned() {
+                        // not registered onchain, submit registration transaction
                         miner_status.set(MinerStatus::Registering);
                         submit_transaction(tx);
                     }
@@ -183,9 +211,9 @@ fn MinerHashpower() -> Element {
 
 fn MinerPendingRewards() -> Element {
     let member = use_member();
-    let member_record = use_member_record();
+    let member_record_balance = use_member_record_balance();
     rsx! {
-        if let Some(Ok(member_record)) = member_record.cloned() {
+        if let Some(Ok(member_record_balance)) = member_record_balance.cloned() {
             Col {
                 gap: 4,
                 span {
@@ -196,7 +224,7 @@ fn MinerPendingRewards() -> Element {
                     if let Ok(member) = member {
                         OreValue {
                             size: TokenValueSize::Large,
-                            ui_amount_string: amount_to_ui_amount_string(member_record.total_balance as u64 - member.total_balance, TOKEN_DECIMALS),
+                            ui_amount_string: amount_to_ui_amount_string(member_record_balance - member.total_balance, TOKEN_DECIMALS),
                             with_decimal_units: true,
                         }
                     } else {
