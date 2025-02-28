8d0a0388e19a55c92fcb1322fd3d091a95e3fae5
Alexander Binaei <aabinaei@gmail.com>
2025-02-28 12:39:45 -0800

    with logs

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,42 @@ const CSS: &str = include_str!("../public/tailwind.css");
 fn main() {
     #[cfg(feature = "web")]
     wasm_logger::init(wasm_logger::Config::default());
-    #[cfg(not(feature = "web"))]
-    env_logger::init();
+
+    use directories::ProjectDirs;
+    use log::{info, warn};
+    use std::fs::{create_dir_all, OpenOptions};
+
+    use fern::Dispatch;
+    use log::LevelFilter;
+    use std::fs;
+    use std::path::PathBuf;
+
+    // Let's say your app's "organization" is "myorg" and app name is "MyApp".
+    // The domain qualifier can be "com", or your domain, etc.
+    let proj_dirs =
+        ProjectDirs::from("ore", "supply", "ore-app").expect("Could not determine home directory");
+
+    // For logs, it's common to use `cache_dir` or `data_local_dir`, but choose whichever fits:
+    let log_dir: PathBuf = proj_dirs.cache_dir().join("logs");
+    fs::create_dir_all(&log_dir).expect("Could not create log directory");
+
+    let log_file_path = log_dir.join("my_logs.log");
+
+    // Use fern (or another crate) to write logs to file (and optionally to stdout too)
+    Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}] {}",
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(LevelFilter::Info)
+        .chain(fern::log_file(&log_file_path).expect("Failed to open log file"))
+        .apply()
+        .expect("Failed to set up logging");
+
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     launch(App);
 }
