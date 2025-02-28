b9d88d3d3a7516eaa9c8d3ccdc848eb3bb650c5f
Alexander Binaei <aabinaei@gmail.com>
2025-02-27 22:34:12 -0800

    download with resource

--- src/components/updater/mod.rs
+++ src/components/updater/mod.rs
@@ -11,88 +11,94 @@ const ENDPOINT: &str = "https://api.ore.supply/app/update/{{target}}/{{arch}}/{{
 const PUBKEY: &str = "dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IEE1RkFDQUFCQ0M0NDhBRTQKUldUa2lrVE1xOHI2cGJSaXdCS0NVWGdBQTYzSGFNTXlBRlc5NThYVFhwUEVab29UaGpiSk1WWloK";
 
 pub fn Updater() -> Element {
-    let signal = use_signal(|| State::AlreadyHaveLatest);
-    updater(signal);
+    let update = updater();
     rsx! {
-        match signal.cloned() {
-            State::AlreadyHaveLatest => {
-                rsx! {}
-            }
-            State::UpdateAvailable(update, binary) => {
-                let update = update.clone();
-                let binary = Arc::clone(&binary);
-                rsx! {
-                    button {
-                        class: "text-xl",
-                        onclick: move |_| {
-                            let update = update.clone();
-                            let binary = Arc::clone(&binary);
-                            spawn(async move {
-                                if let Err(err) = update.install(binary.as_ref()) {
-                                    log::error!("{:?}", err);
-                                }
-                            });
-                        },
-                        "update?"
+        match &*update.read() {
+            Some(Ok(state)) => {
+                match state {
+                    State::AlreadyHaveLatest => {
+                        rsx! {}
+                    }
+                    State::UpdateAvailable(update, binary) => {
+                        let update = update.clone();
+                        let binary = Arc::clone(&binary);
+                        rsx! {
+                            button {
+                                class: "text-xl",
+                                onclick: move |_| {
+                                    log::info!("button clicked");
+                                    let update = update.clone();
+                                    let binary = Arc::clone(&binary);
+                                    spawn(async move {
+                                        log::info!("installing");
+                                        if let Err(_err) = update.install(binary.as_ref()) {
+                                            log::error!("error installing binary");
+                                        }
+                                    });
+                                },
+                                "update?"
+                            }
+                        }
                     }
                 }
+            }
+            _ => {
+                rsx! {}
             }
         }
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum State {
     AlreadyHaveLatest,
     UpdateAvailable(Update, Arc<NewBinaryToInstall>),
 }
 type NewBinaryToInstall = Vec<u8>;
 
-fn updater(mut signal: Signal<State>) -> Result<()> {
-    let endpoint = Url::parse(ENDPOINT)?;
-    // signer pubkey
-    let pubkey = String::from(PUBKEY);
-    // config
-    let config = Config {
-        endpoints: vec![endpoint],
-        pubkey,
-        ..Default::default()
-    };
-    // current version for reference
-    let current_version = env!("CARGO_PKG_VERSION");
-    let current_version = Version::parse(current_version)?;
-    // channel
-    let (sender, receiver) = tokio::sync::oneshot::channel::<State>();
-    // check for update
-    let handle = std::thread::spawn(move || {
-        let update = cargo_packager_updater::check_update(current_version, config);
-        let state = if let Ok(Some(update)) = update {
-            log::info!("update: {:?}", update);
-            // download
-            if let Ok(bytes) = update.download() {
-                State::UpdateAvailable(update, Arc::new(bytes))
-            } else {
-                log::error!("failed to download latest update");
-                State::AlreadyHaveLatest
-            }
-        } else {
-            log::info!("no update available");
-            State::AlreadyHaveLatest
+fn updater() -> Resource<Result<State>> {
+    use_resource(move || async move {
+        let endpoint = Url::parse(ENDPOINT)?;
+        // signer pubkey
+        let pubkey = String::from(PUBKEY);
+        // config
+        let config = Config {
+            endpoints: vec![endpoint],
+            pubkey,
+            ..Default::default()
         };
-        if let Err(err) = sender.send(state) {
-            log::error!("{:?}", err);
-        }
-    });
-    // spawn dioxus thread to listen for update
-    spawn(async move {
-        match receiver.await {
-            Ok(state) => {
-                signal.set(state);
+        // current version for reference
+        let current_version = env!("CARGO_PKG_VERSION");
+        let current_version = Version::parse(current_version)?;
+        // update channel --
+        // the updater is implemented with a blocking client
+        // so to fetch the binary to install without blocking the UI
+        // we need to spawn a system thread
+        // and then send the bytes thru a oneshot channel
+        // which supports a sync sender *and* async reader
+        let (sender, receiver) = tokio::sync::oneshot::channel::<State>();
+        // check for update
+        let handle = std::thread::spawn(move || {
+            let update = cargo_packager_updater::check_update(current_version, config);
+            let state = if let Ok(Some(update)) = update {
+                log::info!("update found: {:?}", update);
+                // download
+                if let Ok(bytes) = update.download() {
+                    log::info!("update downloaded");
+                    State::UpdateAvailable(update, Arc::new(bytes))
+                } else {
+                    log::error!("failed to download latest update");
+                    State::AlreadyHaveLatest
+                }
+            } else {
+                log::info!("no update available");
+                State::AlreadyHaveLatest
+            };
+            if let Err(_err) = sender.send(state) {
+                log::error!("error sending download to signal");
             }
-            Err(err) => {
-                log::error!("{:?}", err);
-            }
-        }
-    });
-    Ok(())
+        });
+        // spawn dioxus thread to listen for update
+        receiver.await.map_err(|err| anyhow::anyhow!(err))
+    })
 }
