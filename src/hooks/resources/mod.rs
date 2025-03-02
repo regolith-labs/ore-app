5392bef475520271c3aa07fed8422b1fbf6357f8
Marco <tmiwia@gmail.com>
2025-03-01 17:01:43 -0800

    Move tx constants to transaction builders folder

--- src/hooks/resources/mod.rs
+++ src/hooks/resources/mod.rs
@@ -18,10 +18,6 @@ pub use use_ore_price::*;
 pub use use_stake::*;
 pub use use_token_balance::*;
 
-pub const MIN_SOL_BALANCE: f64 = 0.1;
-pub const APP_FEE: u64 = 5_000;
-pub const SOLANA_BASE_FEE: u64 = 5_000;
-
 pub fn use_cache_provider() {
     use_boosts_provider();
     use_liquidity_pairs_provider();
@@ -30,5 +26,4 @@ pub fn use_cache_provider() {
     use_members_provider();
     use_boost_yield_provider();
     use_token_balance_provider();
-    // TODO ADD TOKEENS
 }
