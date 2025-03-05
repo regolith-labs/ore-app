0fd9df504f28b3a9350504002006390e08f85182
Hardhat Chad <hardhatchad@gmail.com>
2025-03-04 21:46:24 -0800

    landing copy

--- src/pages/stake.rs
+++ src/pages/stake.rs
@@ -17,7 +17,7 @@ pub fn Stake() -> Element {
             Heading {
                 class: "mx-auto w-full max-w-2xl px-5 sm:px-8",
                 title: "Stake",
-                subtitle: "Provide liquidity and earn native yield."
+                subtitle: "Provide liquidity and earn yield."
             }
             Col {
                 gap: 16,
