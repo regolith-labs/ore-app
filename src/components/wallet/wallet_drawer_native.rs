95612a6e4e723a334fe9c1541f4ad1205086ba97
Alexander Binaei <aabinaei@gmail.com>
2025-03-06 14:34:03 -0800

    pk copied

--- src/components/wallet/wallet_drawer_native.rs
+++ src/components/wallet/wallet_drawer_native.rs
@@ -3,17 +3,22 @@ use std::str::FromStr;
 use dioxus::prelude::*;
 use dioxus_sdk::clipboard::use_clipboard;
 
-use crate::hooks::{on_transaction_done, use_wallet, use_wallet_native, Wallet};
+use crate::hooks::{use_wallet, use_wallet_native, Wallet};
 
 #[component]
 pub fn WalletDrawer(on_close: EventHandler<MouseEvent>) -> Element {
+    // clipboard
+    let mut clipboard = use_clipboard();
     // wallet
     let wallet = use_wallet();
+    // pubkey
     let mut pubkey = use_signal(|| "missing pubkey".to_string());
     let mut pubkey_splice = use_signal(|| Splice::Pubkey("0000...0000".to_string()));
-    let mut keypair_export = use_signal(|| "failed to export private key".to_string());
-    let mut is_copied = use_signal(|| false);
+    let mut pubkey_copied = use_signal(|| false);
+    // keypair
+    let mut keypair = use_signal(|| "failed to read private key".to_string());
     let mut show_keypair_export = use_signal(|| false);
+    let mut keypair_copied = use_signal(|| false);
     // listen for wallet update
     use_memo(move || {
         if let Wallet::Connected(pk) = *wallet.read() {
@@ -26,9 +31,7 @@ pub fn WalletDrawer(on_close: EventHandler<MouseEvent>) -> Element {
             }
         }
     });
-    // clipboard
-    let mut clipboard = use_clipboard();
-    // listen for clipboard
+    // listen for pubkey clipboard
     use_memo(move || {
         if let Splice::Copied = *pubkey_splice.read() {
             spawn(async move {
@@ -40,17 +43,42 @@ pub fn WalletDrawer(on_close: EventHandler<MouseEvent>) -> Element {
             });
         }
     });
+    // listen for pubkey copied
+    use_memo(move || {
+        if *pubkey_copied.read() {
+            spawn(async move {
+                tokio::time::sleep(tokio::time::Duration::from_millis(1_500)).await;
+                pubkey_copied.set(false);
+            });
+        }
+    });
+    // listen for keypair copied
+    use_memo(move || {
+        if *keypair_copied.read() {
+            spawn(async move {
+                tokio::time::sleep(tokio::time::Duration::from_millis(1_500)).await;
+                keypair_copied.set(false);
+            });
+        }
+    });
     // listen for keypair export
     use_memo(move || {
         if *show_keypair_export.read() {
-            if let Ok(keypair) = use_wallet_native::get() {
-                let keypair = keypair.creator.to_base58_string();
-                keypair_export.set(keypair.clone());
+            if let Ok(kp) = use_wallet_native::get() {
+                let kp = kp.creator.to_base58_string();
+                keypair.set(kp.clone());
             }
+        } else {
+            // clear keypair so that it's not sitting in memory
+            keypair.set("failed to read private key".to_string());
         }
     });
     rsx! {
         div { class: "flex flex-col gap-8 h-full sm:w-96 w-screen elevated elevated-border text-white py-8 z-50",
+            onclick: move |e| {
+                e.stop_propagation();
+                show_keypair_export.set(false);
+            },
             button {
                 class: "flex justify-center items-center rounded-full text-center py-4 px-6 mx-4 controls-secondary hover:cursor-pointer",
                 onclick: move |e| {
@@ -68,17 +96,24 @@ pub fn WalletDrawer(on_close: EventHandler<MouseEvent>) -> Element {
                     div {
                         class: "p-4 controls-secondary break-all text-sm w-full",
                         style: "word-break: break-word; white-space: pre-wrap;",
-                        "{keypair_export}"
+                        "{keypair.read().to_string()}"
                     }
                     button {
                         class: "p-2 controls-secondary text-center text-sm w-full hover:cursor-pointer flex justify-center items-center",
                         onclick: move |e| {
                             e.stop_propagation();
-                            if let Err(err) = clipboard.set(keypair_export.to_string()) {
+                            if let Err(err) = clipboard.set(keypair.to_string()) {
                                 log::error!("failed to set clipboard: {:?}", err);
                             }
+                            keypair_copied.set(true);
                         },
-                        "Copy"
+                        div {
+                            if *keypair_copied.read() {
+                                "Copied!"
+                            } else {
+                                "Copy"
+                            }
+                        }
                     }
                 }
             } else {
