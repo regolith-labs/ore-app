264ca4a76d71ecef40080b7297c444eb6ef06373
Alexander Binaei <aabinaei@gmail.com>
2025-03-09 22:59:18 -0700

    Code TLC

--- src/components/wallet/mod.rs
+++ src/components/wallet/mod.rs
@@ -1,8 +1,8 @@
-// #[cfg(not(feature = "web"))]
+#[cfg(not(feature = "web"))]
 mod wallet_adapter_native;
 #[cfg(feature = "web")]
 mod wallet_adapter_web;
-// #[cfg(not(feature = "web"))]
+#[cfg(not(feature = "web"))]
 mod wallet_drawer_native;
 #[cfg(feature = "web")]
 mod wallet_drawer_web;
