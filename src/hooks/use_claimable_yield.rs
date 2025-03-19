a124e968b1b7d1cbc779f6a9300296a764e2fb3d
Hardhat Chad <hardhatchad@gmail.com>
2025-03-19 08:43:06 -0700

    tidy

--- src/hooks/use_claimable_yield.rs
+++ src/hooks/use_claimable_yield.rs
@@ -29,12 +29,10 @@ pub fn calculate_claimable_yield(boost: Boost, boost_proof: Proof, stake: Stake)
     if boost_proof.balance > 0 {
         boost_rewards_factor += Numeric::from_fraction(boost_proof.balance, boost.total_deposits);
     }
-
     if boost_rewards_factor > stake.last_rewards_factor {
         let accumulated_rewards = boost_rewards_factor - stake.last_rewards_factor;
         let personal_rewards = accumulated_rewards * Numeric::from_u64(stake.balance);
         rewards += personal_rewards.to_u64();
     }
-
     rewards
 }
