beb4723da52f56f36ab9725e1522eb1b6d9e4bca
Hardhat Chad <hardhatchad@gmail.com>
2025-03-30 19:50:41 -0400

    remove content header

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -24,11 +24,11 @@ async fn main() {
             };
 
             // Set the Content-Type based on the file extension
-            let reply = if path_str.ends_with(".otf") {
-                warp::reply::with_header(reply, CONTENT_TYPE, "font/otf")
-            } else {
-                warp::reply::with_header(reply, CONTENT_TYPE, "")
-            };
+            // let reply = if path_str.ends_with(".otf") {
+            //     warp::reply::with_header(reply, CONTENT_TYPE, "font/otf")
+            // } else {
+            //     warp::reply::with_header(reply, CONTENT_TYPE, "")
+            // };
 
             // Set the Access-Control-Allow-Origin header
             warp::reply::with_header(reply, ACCESS_CONTROL, "*")
