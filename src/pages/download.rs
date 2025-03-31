5a60c5dde48c39a318aec28a30265f8acd5f26c0
Marco <tmiwia@gmail.com>
2025-03-31 01:35:12 -0400

    Upload OS icons | Styling WIP

--- src/pages/download.rs
+++ src/pages/download.rs
@@ -2,7 +2,7 @@ use dioxus::prelude::*;
 
 #[cfg(feature = "web")]
 use crate::{
-    components::{Col, Heading},
+    components::{AppleIcon, Col, DownloadIcon, Heading, Row},
     hooks::{parse_download_url, use_download_url},
 };
 
@@ -26,9 +26,17 @@ pub fn Download() -> Element {
                 subtitle: "Install the ORE desktop app."
             }
 
-            span {
-                class: "text-sm text-gray-500",
-                "System: {os} {arch}"
+            AppleIcon {
+                class: "w-16 h-16"
+            }
+
+            Row {
+                class: "items-center justify-center",
+                gap: 8,
+                DownloadCard {
+                    os: os,
+                    arch: arch
+                }
             }
 
             a {
@@ -37,6 +45,86 @@ pub fn Download() -> Element {
                 span {
                     class: "mx-auto my-auto",
                     "Download"
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn DownloadButton(button_text: String) -> Element {
+    rsx! {
+        Row {
+            class: " sm:flex-row mx-auto md:ml-0 h-min mt-8 px-4 justify-center items-center",
+            gap: 4,
+            Link {
+                to: "/",
+                class: "flex h-12 w-full sm:w-min px-8 rounded-full controls-primary",
+                span {
+                    class: "my-auto mx-auto text-nowrap font-semibold",
+                    "{button_text}"
+                }
+                DownloadIcon {
+                    class: "w-4 sm:w-8 h-4 sm:h-8 m-auto"
+                }
+                // WindowsIcon {
+                //     class: "w-6 sm:w-8 h-6 sm:h-8 m-auto"
+                // }
+
+            }
+        }
+    }
+}
+
+// fn OsIcon(os: String) -> Element {
+//     rsx!{
+//         Link {
+//             to: "https://discord.gg/4TQfshAAsT",
+//             class: "flex h-10 sm:h-12 w-10 sm:w-12 transition-colors rounded-full transition-colors {button_color} hover:bg-controls-secondaryHover my-auto",
+//             new_tab: true,
+//             DiscordIcon {
+//                 class: "w-6 sm:w-8 h-6 sm:h-8 m-auto"
+//             }
+//         }
+//     }
+// }
+
+#[component]
+fn DownloadCard(os: String, arch: String) -> Element {
+    // let class = class.unwrap_or_default();
+    let img_path = "https://pbs.twimg.com/profile_images/1510345561731330063/mRH8nY7D_400x400.jpg";
+    rsx! {
+        Col {
+            class: "w-1/2 justify-center rounded-md p-5 grow",
+
+            Col {
+                class: "gap-3 items-center",
+                img {
+                    class: "w-16 h-16 rounded-full ",
+                    src: "{img_path}"
+                }
+                Col {
+                class: "gap-1",
+                    // p {
+                    //     class: "text-elements-midEmphasis text-center",
+                    //     "Desktop"
+                    // }
+                    span {
+                        class: "font-semibold text-elements-highEmphasis text-center text-3xl",
+                        "{os}"
+                    }
+                    // span {
+                    //     class: "text-sm text-gray-500",
+                    //     "System: {arch}"
+                    // }
+
+                    // p {
+                    //     class: "text-sm text-elements-midEmphasis text-center",
+                    //     "Download the latest version of the ORE desktop app."
+                    // }
+                    // DownloadButton {
+                    //     button_text: "Download".to_string()
+                    // }
                 }
             }
         }
