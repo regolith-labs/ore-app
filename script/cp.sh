9b2036b38599e338aea9393db2383e7500623374
Hardhat Chad <hardhatchad@gmail.com>
2025-04-02 12:46:50 -0400

    confirm pre-deploy script

--- script/cp.sh
+++ script/cp.sh
@@ -2,3 +2,4 @@ cp public/icon.png target/dx/ore-app/release/web/public/assets
 cp public/metadata.json target/dx/ore-app/release/web/public/assets
 cp public/favicon.png target/dx/ore-app/release/web/public/assets
 cp -R public/fonts target/dx/ore-app/release/web/public/assets
+ls -la target/dx/ore-app/release/web/public/assets
