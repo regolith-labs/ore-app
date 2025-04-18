92062d33b7109fb806f8a468a05b738b3c7dd17d
Hardhat Chad <hardhatchad@gmail.com>
2025-04-18 15:30:42 -0700

    promote

--- src/pages/promote.rs
+++ src/pages/promote.rs
@@ -82,3 +82,17 @@ fn Dashboard() -> Element {
         }
     }
 }
+
+fn Waitlist() -> Element {
+    rsx! {
+        Col {
+            class: "w-full h-full pb-20 sm:pb-16",
+            gap: 8,
+            Heading {
+                class: "mx-auto w-full max-w-2xl px-5 sm:px-8",
+                title: "Waitlist",
+                subtitle: "You're on the waitlist."
+            }
+        }
+    }
+}
